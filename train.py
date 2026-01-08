0b02626f53a3b13c531138c61c32273c6ea09ec5
mkenjis <mkenjis@gmail.com>
2026-01-08 15:45:44 -0300

    First POC

--- train.py
+++ train.py
@@ -31,3 +31,24 @@ reval.evaluate(pred_train)
 
 pred_test = pipelinemodel.transform(test)
 reval.evaluate(pred_test)
+
+import mlflow
+import mlflow.spark
+
+mlflow.set_experiment("Advertising_via_github")
+mlflow.set_tracking_uri(uri="http://192.168.0.23:5000/")
+
+with mlflow.start_run(run_name="linear-regression"):
+    # Log model
+    pipelinemodel = pipeline.fit(train)
+    mlflow.spark.log_model(pipelinemodel, "model")
+    # Log params: num_trees and max_depth
+    mlflow.log_param("iterations", pipelinemodel.stages[-1].getMaxIter())
+    mlflow.log_param("regparam", pipelinemodel.stages[-1].getRegParam())
+    predDF = pipelinemodel.transform(test)
+    reval = RegressionEvaluator().setLabelCol("label").setPredictionCol("prediction")
+    # Log metrics: RMSE and R2
+    rmse = reval.setMetricName("rmse").evaluate(predDF)
+    r2 = reval.setMetricName("r2").evaluate(predDF)
+    #r2 = regressionEvaluator.setMetricName("r2").evaluate(predDF)
+    mlflow.log_metrics({"rmse": rmse, "r2": r2 })
